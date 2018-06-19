e76d33e328f93213c2b64517158fcd86fbcede52
wipnoah <wipnoah@gmail.co>
2018-06-19 14:01:22 +0200

    Update
    Added Routes to app.module.ts

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NotesComponent } from './notes/notes.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { AddressBookComponent } from './address-book/address-book.component';
 import { BacklogComponent } from './backlog/backlog.component';
+
+const appRoutes: Routes = [
+  {path: 'notes', component : NotesComponent},
+  {path: 'todo-list', component : TodoListComponent},
+  {path: 'backlog', component : BacklogComponent},
+  {path: 'addressbook', component: AddressBookComponent},
+]
 
 @NgModule({
   declarations: [
@@ -16,7 +24,9 @@ import { BacklogComponent } from './backlog/backlog.component';
     BacklogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true}
+      )
   ],
   providers: [],
   bootstrap: [AppComponent]
